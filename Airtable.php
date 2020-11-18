d1e35716718ee4160fa60bbe0790ef8b009dface
Marc Anton Dahmen <mail@marcdahmen.de>
2020-11-18 12:24:55 +0100

    Create Airtable.php

---
+++ Airtable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *	Automad Airtable
+ *
+ *	An Airtable integration for Automad.
+ *
+ *	@author Marc Anton Dahmen
+ *	@copyright Copyright (C) 2020 Marc Anton Dahmen - <https://marcdahmen.de> 
+ *	@license MIT license
+ */
+
+namespace Automad;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+class Airtable {
+
+
+	/**
+	 *	The main function.
+	 *
+	 *	@param array $options
+	 *	@param object $Automad
+	 *	@return string the output of the extension
+	 */
+
+	public function Airtable($options, $Automad) {
+
+		return;
+
+	}
+
+
+}
