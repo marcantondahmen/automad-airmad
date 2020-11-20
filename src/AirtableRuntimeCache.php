13b669aca3cb99469f0e40d5a22d6b407a5e3412
Marc Anton Dahmen <mail@marcdahmen.de>
2020-11-20 13:12:23 +0100

    Create AirtableRuntimeCache.php

---
+++ src/AirtableRuntimeCache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *	Automad Airtable
+ *
+ *	An Airtable integration for Automad.
+ *
+ *	@author Marc Anton Dahmen
+ *	@copyright Copyright (C) 2020 Marc Anton Dahmen - <https://marcdahmen.de> 
+ *	@license MIT license
+ */
+
+namespace Automad;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+class AirtableRuntimeCache {
+
+	public static $run = false; 
+
+}
