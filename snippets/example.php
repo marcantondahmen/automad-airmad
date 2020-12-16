3da3980f9c3cff3baf7ebb8edb06b557a057a9f3
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-12-16 20:16:44 +0100

    Update example.php

--- snippets/example.php
+++ snippets/example.php
@@ -5,8 +5,24 @@
 	linked: 'Client => Clients',
 	filters: 'Client, Category',
 	template: '/packages/airmad/airmad/snippets/example.handlebars',
-	limit: 1000,
+	limit: 8,
 	prefix: ':example'
 } @>
 
 @{ :exampleOutput }
+
+<ul class="uk-pagination">
+	<@ if @{ ?Page } > 1 @>
+		<li><a href="?<@ queryStringMerge { Page: @{ ?Page | -1 } } @>">←</a></li>
+	<@ end @>
+	<@ for @{ :examplePage | -4 } to @{ :examplePage | +4 } @>
+		<@ if @{ :i } > 0 and @{ :i } <= @{ :examplePages } @>
+			<li><a href="?<@ queryStringMerge { Page: @{ :i } } @>" <@ if @{ ?Page | def(1) } = @{ :i } @>
+				class="uk-active"
+			<@ end @>>@{:i}</a></li>
+		<@ end @>
+	<@ end @>
+	<@ if @{ ?Page } < @{ :examplePages } @>
+		<li><a href="?<@ queryStringMerge { Page: @{ ?Page | +1 } } @>">→</a></li>
+	<@ end @>
+</ul>
